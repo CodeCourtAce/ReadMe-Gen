868d9357858e0c60478170c9fef4e414af5d4c53
Gianna DAconti <themeanmememan@gmail.com>
2024-08-23 23:33:16 -0400

    Added more stuff

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
 
+const {default: inquirer} = require('inquirer');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    name: "name",
+    type: "input",
+    message: "What is your name?"
+
+
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    
+}
 
 // Function call to initialize app
 init();
